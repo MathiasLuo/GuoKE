762f9c0caeb6117dc479e20c0fb7fe094b592232
Luo wuxia <1070753575@qq.com>
2015-03-16 23:12:41 +0800

    ssssssssssssssssss

--- app/src/main/java/com/luo/luowu/myguoke/Login.java
+++ app/src/main/java/com/luo/luowu/myguoke/Login.java
@@ -12,7 +12,6 @@ public class Login extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		initView();
